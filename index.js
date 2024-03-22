84ef745febfbd13b958bef48539abef03deb21ec
djBrandy <dandobrandon0@gmail.com>
2024-03-22 15:50:17 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activityFotSaturday = 'roller-skate') {
+    return `This Saturday, I want to ${activityFotSaturday}!`;
+}
+const mondayWork = function(activityForMonday = 'go to the office') {
+    return `This Monday, I will ${activityForMonday}.`;
+};
+function wrapAdjective(flair = '*') {
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
